31ff5eb2cd694da649c0dc5a517df9b8e1200c8c
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-10-11 18:03:39 +0300

    made changes on the cardview

--- src/app/CardView/index.tsx
+++ src/app/CardView/index.tsx
@@ -1,49 +1,99 @@
-import React from 'react';  
+'use client'
+import React, { useState } from 'react';
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'; 
 
-const CardSection = () => {  
-  const cards = [  
-    {  
-      image: 'firstproject.png',  
-      title: 'Recipe Nutrition Calculator',  
-      description: 'An application that calculates the nutritional information of a recipe.',  
-      link: 'https://nutrition-analyzer.vercel.app/analyzer.html',  
-    },  
-    {  
-      image: '/Images/secondproject.png',  
-      title: 'BMI Calculator',  
-      description: 'Body Mass Index calculator that calculates and categorizes BMI based on user input.',  
-      link: 'https://bmi-calculator-two-gray.vercel.app/',  
-    },  
-    {  
-      image: '/Images/casestudy.png',  
-      title: 'Empowering Rural Teachers',  
-      description: 'A platform for uninterrupted delivery of quality education for teachers in rural areas providing them with essential digital skills.',  
-      link: '/empowering-rural-teachers',  
-    },  
-  ];  
+const CardSection = () => {
+  const cards = [
+    {
+      image: 'firstproject.png',
+      title: 'Recipe Nutrition Calculator',
+      description: 'An application that calculates the nutritional information of a recipe.',
+      link: 'https://nutrition-analyzer.vercel.app/analyzer.html',
+    },
+    {
+      image: '/Images/secondproject.png',
+      title: 'BMI Calculator',
+      description: 'Body Mass Index calculator that calculates and categorizes BMI based on user input.',
+      link: 'https://bmi-calculator-two-gray.vercel.app/',
+    },
+    {
+      image: '/Images/casestudy.png',
+      title: 'Empowering Rural Teachers',
+      description: 'A platform for uninterrupted delivery of quality education for teachers in rural areas providing them with essential digital skills.',
+      link: '/empowering-rural-teachers',
+    },
+    {
+      image: 'firstproject.png',
+      title: 'Recipe Nutrition Calculator',
+      description: 'An application that calculates the nutritional information of a recipe.',
+      link: 'https://nutrition-analyzer.vercel.app/analyzer.html',
+    },
+    {
+      image: '/Images/secondproject.png',
+      title: 'BMI Calculator',
+      description: 'Body Mass Index calculator that calculates and categorizes BMI based on user input.',
+      link: 'https://bmi-calculator-two-gray.vercel.app/',
+    },
+    {
+      image: '/Images/casestudy.png',
+      title: 'Empowering Rural Teachers',
+      description: 'A platform for uninterrupted delivery of quality education for teachers in rural areas providing them with essential digital skills.',
+      link: '/empowering-rural-teachers',
+    },
+  ];
 
-  return (  
-    <div className="mt-[200px] px-4 sm:px-8 md:px-16 lg:px-32" id='projects'>  
-      <h1 className="text-center text-5xl">Projects</h1>  
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-[200px]" id="skills">  
-        {cards.map((card, index) => (  
-          <div  
-            key={index}  
-            className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-2xl transition duration-300 ease-in-out"  
-          >  
-            <img src={card.image} alt={card.title} className="w-full h-48 object-cover" />  
-            <div className="p-6">  
-              <h3 className="text-lg font-bold mb-2">{card.title}</h3>  
-              <p className="text-gray-600 mb-4">{card.description}</p>  
-              <a href={card.link} className="text-blue-500 hover:text-blue-700">  
-                Learn More  
-              </a>  
-            </div>  
-          </div>  
-        ))}  
-      </div>  
-    </div>  
-  );  
-};  
+  const pageTitles = [
+    ["Recipe Nutrition Calculator", "BMI Calculator", "Empowering Rural Teachers"],
+    ["Design Project 1", "Design Project 2", "Design Project 3"],
+  ];
+
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handleNext = () => {
+    if (currentPage < 1) { 
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePrevious = () => {
+    if (currentPage > 0) { 
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const displayedCards = cards.slice(currentPage * 3, currentPage * 3 + 3);
+
+  return (
+    <div className="mt-[200px] px-4 sm:px-8 md:px-16 lg:px-32 relative" id="projects">
+      <h1 className="text-center text-5xl mb-6">
+        {currentPage === 0 ? "Projects" : "Design Projects"}
+      </h1>
+      <div onClick={handlePrevious} className={`cursor-pointer text-3xl text-blue-500 hover:text-blue-700 ${currentPage === 0 ? 'opacity-50 pointer-events-none' : ''} absolute left-4 top-1/2 transform -translate-y-1/2 z-10`}>
+        <GrFormPrevious />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-[100px]" id="skills">
+        {displayedCards.map((card, index) => (
+          <div
+            key={index}
+            className="bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-2xl transition duration-300 ease-in-out">
+            <img src={card.image} alt={card.title} className="w-full h-48 object-cover" />
+            <div className="p-6">
+              <h3 className="text-lg font-bold mb-2">
+                {pageTitles[currentPage][index]}
+              </h3>
+              <p className="text-gray-600 mb-4">{card.description}</p>
+              <a href={card.link} className="text-blue-500 hover:text-blue-700">
+                Learn More
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div onClick={handleNext} className={`cursor-pointer text-3xl text-blue-500 hover:text-blue-700 ${currentPage === 1 ? 'opacity-50 pointer-events-none' : ''} absolute right-4 top-1/2 transform -translate-y-1/2 z-10`}>
+        <GrFormNext />
+      </div>
+    </div>
+  );
+};
 
 export default CardSection;
--- src/app/Navbar/index.tsx
+++ src/app/Navbar/index.tsx
@@ -14,43 +14,42 @@ const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 right-0 h-16 text-base text-black py-4 px-6 w-full flex font-sans items-center z-50 backdrop-blur-md bg-white/50 sm:px-8 md:px-16 lg:px-32">
       <div className="flex items-center w-full justify-between">
-        <div className="flex-shrink-0">
+        <div className="flex-shrink-0 mr-8">
           <h2 className='text-black text-2xl font-bold'>Mwanasha</h2>
         </div>
-        <div className="hidden md:flex gap-8 lg:gap-40 mt-2 text-xl sm:text-lg md:text-xl lg:text-2xl mr-8 lg:mr-40">
+                <div className="hidden md:flex gap-8 lg:gap-40 mt-2 text-xl sm:text-lg md:text-xl lg:text-2xl mr-8 lg:mr-40 ml-auto">
           <NavLink href="#home" onClick={() => { handleScroll("#home"); setIsMenuOpen(false); }}>Home</NavLink>
           <NavLink href="#about" onClick={() => { handleScroll("#about"); setIsMenuOpen(false); }}>About</NavLink>
           <NavLink href="#projects" onClick={() => { handleScroll("#projects"); setIsMenuOpen(false); }}>Projects</NavLink>
           <NavLink href="#skills" onClick={() => { handleScroll("#skills"); setIsMenuOpen(false); }}>Skills</NavLink>
           <NavLink href="#contact" onClick={() => { handleScroll("#contact"); setIsMenuOpen(false); }}>Contact</NavLink>
         </div>
+        
         <div className="md:hidden">
           <button
             onClick={() => setIsMenuOpen(!isMenuOpen)}
-            className="text-black focus:outline-none backdrop-blur-md bg-white/50 p-2 rounded"
-          >
+            className="text-black focus:outline-none backdrop-blur-md bg-white/50 p-2 rounded">
             <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path 
                 strokeLinecap="round" 
                 strokeLinejoin="round" 
                 strokeWidth={2} 
-                d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} 
-              />
+                d={isMenuOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"} />
             </svg>
           </button>
         </div>
       </div>
+
       {isMenuOpen && (
-        <div className="md:hidden fixed inset-0 bg-white/80 backdrop-blur-md z-50 flex flex-col items-center justify-start pb-12 h-screen"> 
+        <div className="md:hidden fixed inset-0 bg-white/80 backdrop-blur-md z-50 flex flex-col items-center justify-start pb-12 h-screen">
           <button
             onClick={() => setIsMenuOpen(false)}
-            className="absolute top-4 right-4 text-black focus:outline-none p-2 rounded"
-          >
+            className="absolute top-4 right-4 text-black focus:outline-none p-2 rounded">
             <svg className="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
             </svg>
           </button>
-          <div className="flex-grow flex flex-col items-center justify-center"> 
+          <div className="flex-grow flex flex-col items-center justify-center">
             <MobileNavLink href="#home" onClick={() => { handleScroll("#home"); setIsMenuOpen(false); }}>Home</MobileNavLink>
             <MobileNavLink href="#about" onClick={() => { handleScroll("#about"); setIsMenuOpen(false); }}>About</MobileNavLink>
             <MobileNavLink href="#projects" onClick={() => { handleScroll("#projects"); setIsMenuOpen(false); }}>Projects</MobileNavLink>
@@ -62,41 +61,34 @@ const Navbar = () => {
     </nav>
   );
 };
-
 interface NavLinkProps {
   href: string;
   children: ReactNode;
   onClick?: () => void;
 }
-
 function NavLink({ href, children, onClick }: NavLinkProps) {
   return (
     <a 
       href={href} 
       className="text-primary-amber hover:text-blue font-bold"
-      onClick={onClick}
-    >
+      onClick={onClick}>
       {children}
     </a>
   );
 }
-
 interface MobileNavLinkProps {
   href: string;
   children: ReactNode;
   onClick: () => void;
 }
-
 function MobileNavLink({ href, children, onClick }: MobileNavLinkProps) {
   return (
     <a 
       href={href} 
       onClick={onClick} 
-      className="block px-3 py-4 text-xl font-bold text-primary-amber hover:text-blue"
-    >
+      className="block px-3 py-4 text-xl font-bold text-primary-amber hover:text-blue" >
       {children}
     </a>
   );
 }
-
 export default Navbar;
